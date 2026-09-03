a222aa064928a97fca14d79282289279c25879fd
codeharshit-ty <hhsingh2011@gmail.com>
2026-09-03 16:07:20 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,15 @@
+
+class string {
+    public static void main(String[] args) {
+        String first = "Cod";
+        String secound = "ingal";
+        String CodingalTricks = "welcome"+"to"+"codingal";
+        String Codingal = first+secound;    
+        String Codingalcapital = Codingal.toUpperCase();
+        String CodingalSmall = Codingalcapital.toLowerCase();
+        int lengthofcodingal = Codingal.length();
+        System.out.println(CodingalTricks);
+        System.out.println(CodingalSmall);
+        System.out.println(lengthofcodingal);
+    }
+}
